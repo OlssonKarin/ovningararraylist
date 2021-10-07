cccbb2177c806ca97c7baa0d11d69ad53e3aa865
Karin <karin_m_olsson@hotmail.com>
2021-10-07 10:41:53 +0200

    first commit

---
+++ src/main/java/se/karin/App.java
@@ -0,0 +1,19 @@
+package se.karin;
+
+/**
+ * Hello world!
+ */
+public final class App {
+    private App() {
+    }
+
+    /**
+     * Says hello to the world.
+     * @param args The arguments of the program.
+     */
+    public static void main(String[] args) {
+        
+        Lab1 lab1 = new Lab1();
+        lab1.Run();
+    }
+}
---
+++ src/main/java/se/karin/Lab1.java
@@ -0,0 +1,17 @@
+package se.karin;
+
+public class Lab1 {
+    public void Run() {
+
+        int [] num = {12, 55, 32,9,34};
+
+        
+
+
+
+
+    }
+    
+}
+//Skapa en ARRAY i kod med INTS 12 ,55, 32, 9, 34.   
+//Ta fram vilket som är det STÖRSTA och MINSTA talet i arrayen
