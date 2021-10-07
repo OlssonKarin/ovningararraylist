35498d9407f9b86b78adaf3464549ae83f9bc61e
Karin <karin_m_olsson@hotmail.com>
2021-10-07 11:20:42 +0200

    forloop

--- src/main/java/se/karin/Lab1.java
+++ src/main/java/se/karin/Lab1.java
@@ -3,12 +3,19 @@ package se.karin;
 public class Lab1 {
     public void Run() {
 
-        int [] num = {12, 55, 32,9,34};
+        int []talen = {12, 55, 32, 9, 34};
+        int smallest = talen [0];
+        int largest = talen [0];
 
-        
-
-
-
+        for (int tal : talen) {
+          if(tal < smallest){
+            smallest = tal;
+          }
+          if(tal > largest){
+            largest = tal;
+            }
+        }
+        System.out.printf("Det största talet: %d \nDet minsta talet är: %d", largest, smallest);
 
     }
     
